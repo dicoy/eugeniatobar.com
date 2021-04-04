6f9a60b11c953fb451e5db0079be5e52308ecfc7
chili <mctobar@gmail.com>
2021-04-04 16:16:31 -0300

    added euge banner

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <div className={styles.banner />
+        <div className={styles.banner} />
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
