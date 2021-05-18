cff365279519fa86caf5756b19e65d3ea483b35e
chili <mctobar@gmail.com>
2021-05-18 20:13:06 -0300

    logo

--- pages/index.js
+++ pages/index.js
@@ -39,24 +39,24 @@ export default function Home() {
     <header className="near-black dt vh-100 w-100 bg-light-gray cover logo bg-center">
       <div className="dtc v-mid tc w-100">
         <h1 className="f1 lh-solid mw8 center tc b mt6 ph5-ns">Consultant - Showcase your consulting agency/company!</h1>
-        <hr className="mw4 center bn bg-blue" style={{height: '5px'}} />
+        <hr className="mw4 center bn bg-hot-pink" style={{height: '5px'}} />
         <p className="f3 mw8 center tc ph5">Tachyons Templates is a great way to get started building websites using the tachyons css framework. Simply download and launch your site within minutes.</p>
-        <a href="#about" className="f3 ph4 pv3 mb2 dib link no-underline br-pill dim bg-blue ba b--blue near-white page-scroll">Learn More</a>
+        <a href="#about" className="f3 ph4 pv3 mb2 dib link no-underline br-pill dim bg-hot-pink ba b--hot-pink near-white page-scroll">Learn More</a>
       </div>
     </header>
-    <section id="about" className="vh-75 dt bg-blue near-white w-100">
+    <section id="about" className="vh-75 dt bg-hot-pink near-white w-100">
       <div className="dtc v-mid center measure tc">
         <h2 className="f2 f1-ns">Learn what all the fuzz is about!</h2>
         <hr className="mw4 center bn bg-near-white" style={{height: '5px'}} />
         <p className="f4 mw7 center">Tachyons Templates has everything you need to get started showing off your company. Simply copy this page, update the links and contact information, then start showing off.</p>
-        <a href="#services" className="f3 ph4 pv3 mb2 dib link no-underline br-pill dim bg-near-white ba b--near-white blue page-scroll">See Our Services</a>
+        <a href="#services" className="f3 ph4 pv3 mb2 dib link no-underline br-pill dim bg-near-white ba b--near-white hot-pink page-scroll">See Our Services</a>
       </div>
     </section>
 
     <section id="services" className="mv3 pa0">
       <div className="tc mv4 pa0">
         <h2 className="f3 f1-ns">View our Services</h2>
-        <hr className="mw4 center bn bg-blue" style={{height: '5px'}} />
+        <hr className="mw4 center bn bg-hot-pink" style={{height: '5px'}} />
 
       </div>
       <div className="cf w-100 w-80-ns center">
@@ -100,7 +100,7 @@ export default function Home() {
     <div className="cf">
       
       <div className="fl w-50 w-third-ns shadow-hover hide-child">
-        <div className="child cf w-100 h-100 absolute bg-blue tc white f3">
+        <div className="child cf w-100 h-100 absolute bg-hot-pink tc white f3">
           <div className="dt w-100 h-100">
             <div className="dtc v-mid">
               <h2 className="f4 f2-ns ma0 pa0 fw6 tc near-white">Frank Ocean Blonde Album Cover</h2>
@@ -114,7 +114,7 @@ export default function Home() {
       </div>
       
       <div className="fl w-50 w-third-ns shadow-hover hide-child">
-        <div className="child cf w-100 h-100 absolute bg-blue tc white f3">
+        <div className="child cf w-100 h-100 absolute bg-hot-pink tc white f3">
           <div className="dt w-100 h-100">
             <div className="dtc v-mid">
               <h2 className="f4 f2-ns ma0 pa0 fw6 tc near-white">Young Thug - Jeffery Album Cover</h2>
@@ -128,7 +128,7 @@ export default function Home() {
       </div>
       
       <div className="fl w-50 w-third-ns shadow-hover hide-child">
-        <div className="child cf w-100 h-100 absolute bg-blue tc white f3">
+        <div className="child cf w-100 h-100 absolute bg-hot-pink tc white f3">
           <div className="dt w-100 h-100">
             <div className="dtc v-mid">
               <h2 className="f4 f2-ns ma0 pa0 fw6 tc near-white">Kendrick Lamar untitled unmastered. Album Cover</h2>
@@ -142,7 +142,7 @@ export default function Home() {
       </div>
       
       <div className="fl w-50 w-third-ns shadow-hover hide-child">
-        <div className="child cf w-100 h-100 absolute bg-blue tc white f3">
+        <div className="child cf w-100 h-100 absolute bg-hot-pink tc white f3">
           <div className="dt w-100 h-100">
             <div className="dtc v-mid">
               <h2 className="f4 f2-ns ma0 pa0 fw6 tc near-white">Kaytranada 99%</h2>
@@ -156,7 +156,7 @@ export default function Home() {
       </div>
       
       <div className="fl w-50 w-third-ns shadow-hover hide-child">
-        <div className="child cf w-100 h-100 absolute bg-blue tc white f3">
+        <div className="child cf w-100 h-100 absolute bg-hot-pink tc white f3">
           <div className="dt w-100 h-100">
             <div className="dtc v-mid">
               <h2 className="f4 f2-ns ma0 pa0 fw6 tc near-white">Baauer Aa - Album Cover</h2>
@@ -170,7 +170,7 @@ export default function Home() {
       </div>
       
       <div className="fl w-50 w-third-ns shadow-hover hide-child">
-        <div className="child cf w-100 h-100 absolute bg-blue tc white f3">
+        <div className="child cf w-100 h-100 absolute bg-hot-pink tc white f3">
           <div className="dt w-100 h-100">
             <div className="dtc v-mid">
               <h2 className="f4 f2-ns ma0 pa0 fw6 tc near-white">Santigold 99 cents - Album cover</h2>
@@ -195,14 +195,14 @@ export default function Home() {
     <section id="contact" className="vh-75 w-100 dt contact bg-near-white">
       <div className="dtc v-mid">
         <h2 className="tc near-black f1">Lets get in Touch!</h2>
-        <hr className="mw4 center bn bg-blue" style={{height: '5px' }} />
+        <hr className="mw4 center bn bg-hot-pink" style={{height: '5px' }} />
 
         <p className="mw7 center b near-black lh-copy f5 tc">
           Contact a teammate to boost your sales and fire up your next project.
           Shoot us an email or give us a shout and we'll get back asap!
         </p>
         <div className="pv4 ph3 tc">
-          <a className="link blue hover-silver dib mh3 tc" href="mailto:feedback@tachyonstemplates.com?subject=feedback" title="email">
+          <a className="link hot-pink hover-silver dib mh3 tc" href="mailto:feedback@tachyonstemplates.com?subject=feedback" title="email">
             <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
         <polyline points="22,6 12,13 2,6"></polyline>
@@ -210,7 +210,7 @@ export default function Home() {
 
             <span className="f5 b db">email us</span>
           </a>
-          <a className="link blue hover-silver dib mh3 tc" href="tel:123-456-7899" title="GitHub">
+          <a className="link hot-pink hover-silver dib mh3 tc" href="tel:123-456-7899" title="GitHub">
             <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none" stroke="#000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
     </svg>
